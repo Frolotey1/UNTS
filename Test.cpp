2d348b069726d1b9235d84ee3f901ba78a66748e
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2025-11-16 04:53:23 +0500

    Update Test.cpp
    Test file with ready header file of project

--- Test.cpp
+++ Test.cpp
@@ -1,14 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include "unts.h"
 
-
 int main() {
-    unts::unique_array<int,10> array;
-    array.add(0,1);
-    array.add(1,2);
-    array.add(2,3);
-    array.add(3,4);
-    array.for_each();
-
-    return 0 ;
+    std::cout << "Hello World";
+    return 0;
 }
